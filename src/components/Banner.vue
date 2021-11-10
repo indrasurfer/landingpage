6a6d7fc5ba086d56bca88d93dd456df61dd0efc3
papancode <49733365+papancode@users.noreply.github.com>
2021-11-10 11:32:39 +0700

    revisi
    revisi

---
+++ src/components/Banner.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <header class="masthead">
+         <img src="../assets/images/bannerkiri.png" class="header masthead masthead-kiri-1" alt=""/>
+         <div class="banner-one__bg hero">
+           </div>
+            <div class="banner-one__bg" style="background-image:url(/assets/bannerkanan.jpg)">
+                </div>
+            <div class="container">
+               
+                <div class="banner-one__video video-popup">
+                  <i fa fa-play> </i>
+                </div>
+                <div class="banner-one__moc" href="#services">
+                  <img src="../assets/images/hp.png" class="wow fadeInUp" data-wow-duration="1500ms" alt=""/>
+                </div>
+                <div class="row1">
+                 <div class="col-lg-7">
+                   <div class="banner-one__content"> 
+                     <form class="banner-one__mc-form mc-form">
+                       <input type="text" name="email" placeholder="Email address">
+                       <button type="submit" class="btn banner-one__mc-btn"><span>Free Trial</span></button>
+                     </form>
+                     <div class="mc-form__response"></div>
+                     <h3>Manage Your <br> Project with <br> Application</h3>
+                     <p>Nulla facilisi. Proin felis neque, suscipit egestas erat a<!-- --> <br> tincidunt finibus magna consectetur lacus.</p>
+                     <a href="#" class="btn banner-one__btn"><span>Discover More</span></a>
+                   </div>
+                 </div> 
+                </div>
+            </div>
+         
+    </header>
+    <br>
+    <div>
+  </div>
+  </div>
+
+
+  
+</template>
+
+<script>
+</script>
+<style lang="scss" scoped>
+
+    .hero {
+        background-image: url('../assets/images/kanan.jpg');
+    }
+
+</style>
